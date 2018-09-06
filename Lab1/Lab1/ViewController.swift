235a603e72bf64dea80886b2d5ac2eeac2ee02df
OlhaPolishchuk <OlhaPolishchuk@users.noreply.github.com>
2018-09-06 12:57:44 +0300

    updated Lab1-2

--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -11,17 +11,4 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var label: UIView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
--- Lab2/Lab2/ViewController.swift
+++ Lab2/Lab2/ViewController.swift
@@ -14,20 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var input: UITextField!
     
-    
     @IBAction func action(_ sender: UIButton) {
         output.text = "Hello, " + (input.text)! + "!"
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
