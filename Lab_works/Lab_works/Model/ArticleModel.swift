a11f3fd52e96d86f93beee898df2e346cc0f3649
OlhaPolishchuk <OlhaPolishchuk@users.noreply.github.com>
2018-10-17 20:35:01 +0300

    added Lab5

---
+++ Lab_works/Lab_works/Model/ArticleModel.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleModel.swift
+//  Lab_works
+//
+//  Created by Olha Polishchuk on 17.10.18.
+//  Copyright © 2018 Olga. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleModel{
+    var author: String
+    var title: String
+    var urlToImage: String
+}
