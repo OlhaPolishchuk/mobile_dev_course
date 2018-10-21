72028b84fb8f81c4beb666b2a8adad72ae9f8a1d
OlhaPolishchuk <OlhaPolishchuk@users.noreply.github.com>
2018-10-21 22:43:16 +0300

    update lab5

---
+++ Lab_works/Lab_works/Model/ApiManager.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+private let API_URL = URL(string: "https://newsapi.org/v2/everything?domains=nationalgeographic.com&apiKey=7e11ca926e534fefa89e0cb71c19acb8")!
+
+class ApiManager{
+    var articlesData: [ArticleModel] = []
+    private let API_URL = URL(string: "https://newsapi.org/v2/everything?domains=nationalgeographic.com&apiKey=7e11ca926e534fefa89e0cb71c19acb8")!
+    
+    public func fetchPhotosData(){
+        DispatchQueue.main.async {
+            Alamofire.request(self.API_URL).responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    json["articles"].array?.forEach({ (article) in
+                        let article = ArticleModel(author: article["author"].stringValue, title: article["title"].stringValue, urlToImage: article["urlToImage"].stringValue)
+                        self.articlesData.append(article)
+                    })
+                    print(self.articlesData)
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
+
