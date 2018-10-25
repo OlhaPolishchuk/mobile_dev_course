29e73fac4c7308ca9f565ef24bb88b3c7981b91e
OlhaPolishchuk <OlhaPolishchuk@users.noreply.github.com>
2018-10-25 10:23:24 +0300

    final update lab5

--- Lab_works/Lab_works/Model/ApiManager.swift
+++ Lab_works/Lab_works/Model/ApiManager.swift
@@ -6,23 +6,26 @@ class ApiManager{
     private let API_URL = URL(string: "https://newsapi.org/v2/everything?domains=nationalgeographic.com&apiKey=7e11ca926e534fefa89e0cb71c19acb8")!
     
     public func fetchPhotosData(){
-        var articlesData: [ArticleModel] = []
         DispatchQueue.main.async {
             Alamofire.request(self.API_URL).responseJSON { response in
                 switch response.result {
                 case .success(let value):
-                    let json = JSON(value)
-                    json["articles"].array?.forEach({ (article) in
-                        let article = ArticleModel(author: article["author"].stringValue, title: article["title"].stringValue, urlToImage: article["urlToImage"].stringValue)
-                        articlesData.append(article)
-                    })
-                    print(articlesData)
-                    
+                    self.extractedFunc(value)
                 case .failure(let error):
                     print(error.localizedDescription)
                 }
             }
         }
+    }
+   
+    fileprivate func extractedFunc(_ value: (Any)) {
+        var articlesData: [ArticleModel] = []
+        let json = JSON(value)
+        json["articles"].array?.forEach({ (article) in
+            let article = ArticleModel(author: article["author"].stringValue, title: article["title"].stringValue, urlToImage: article["urlToImage"].stringValue)
+            articlesData.append(article)
+        })
+        print(articlesData)
     }
 }
 
